3c105a6d38e53b3cbfa064d84bc3e3ad5aade31b
Luis Rodriguez <luis.rodriguez@dixmit.com>
2024-10-01 17:03:33 +0200

    Revert "[IMP] agreement_serviceprofile"
    This reverts commit 1ca7217b78821d2e69f9a0c94b1804765b9c0c9a.

--- agreement_serviceprofile/models/agreement_serviceprofile.py
+++ agreement_serviceprofile/models/agreement_serviceprofile.py
@@ -26,10 +26,7 @@ class AgreementServiceProfile(models.Model):
 
     notes = fields.Text(string="Notes")
     product_id = fields.Many2one('product.template', 'Service',
-                                 domain="[('type', '=', 'service')]",
-                                 required=True)
-    partner_id = fields.Many2one(related='agreement_id.partner_id',
-                                 string='Partner')
+                                 domain="[('type', '=', 'service')]")
 
     # Used for Kanban grouped_by view
     @api.model
