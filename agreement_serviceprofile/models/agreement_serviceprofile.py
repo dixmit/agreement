8560b4703279516a4ddfdcab1b090d0928ae5d72
Luis Rodriguez <luis.rodriguez@dixmit.com>
2024-10-01 17:03:31 +0200

    Revert "[IMP] Product Variant on Serviceprofile"
    This reverts commit 13e415e5f8f37a7bc5711b1d74443f8d2fbd0b99.

--- agreement_serviceprofile/models/agreement_serviceprofile.py
+++ agreement_serviceprofile/models/agreement_serviceprofile.py
@@ -28,11 +28,8 @@ class AgreementServiceProfile(models.Model):
     notes = fields.Text(string="Notes")
     product_id = fields.Many2one('product.template', 'Service Product',
                                  domain="[('is_serviceprofile', '=', True), "
-                                        "('type', '=', 'service')]")
-    product_variant_id = fields.Many2one('product.product', 'Service Product Variant',
-                                         domain="[('is_serviceprofile', '=', True), "
-                                                "('type', '=', 'service')]")
-    use_product_variant = fields.Boolean('Use Product Variant', default=False)
+                                        "('type', '=', 'service')]",
+                                 required=True)
     partner_id = fields.Many2one(related='agreement_id.partner_id',
                                  string='Partner')
 
